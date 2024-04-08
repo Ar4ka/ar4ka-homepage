90969c56c8a8a3d8e3dac0dffa9f7ee689837a0f
Ar4ka <rene.juergens01@gmail.com>
2024-04-08 22:26:33 +0200

    feat(footer): started with footer

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <main class="m-auto flex-1 dark:bg-slate-900">
+    <slot />
+  </main>
+  <Footer />
+</template>
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container m-auto">
+    <NuxtPicture
+      format="avif,webp"
+      src="/img/hello_there.jpg"
+      :img-attrs="{ class: 'w-full' }"
+    />
+  </div>
+</template>
